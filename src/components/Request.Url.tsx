4a43d46aa9228b25bafbcb56a90307ec0cffd5ed
onlymachiavelli <tmakaveli643@gmail.com>
2023-01-12 23:38:41 +0100

    request field url

--- src/components/Request.Url.tsx
+++ src/components/Request.Url.tsx
@@ -1,22 +1,32 @@
-import {SendIcon, Down} from './svg'
+import { SendIcon, Down } from "./svg"
 
+const RequestURL = ({ ...props }) => {
+  return (
+    <div className="w-10/12 absolute rounded-full h-auto flex align-center justify-center m-auto bg-ddark items-center flex-wrap m-auto ">
+      <button
+        onClick={props.Method}
+        className="w-1/6 py-3 bg-pink text-white rounded-full  font-bold flex items-center justify-center"
+      >
+        METHOD &nbsp;
+        <Down Width="20" Height="20" />
+      </button>
 
-const RequestURL = ({...props}) =>{
-    return (
-
-
-        <div className="w-10/12 absolute rounded-full h-auto flex align-center justify-center m-auto bg-ddark items-center flex-wrap m-auto ">
-            <button onClick={props.Method} className="px-10 py-3 bg-pink text-white rounded-full  font-bold">
-                METHOD
-            </button>
-
-            <input type={"text"} value={props.Value} onChange={props.OnFunc} className="text-white bg-transparent w-4/5 pl-2" placeholder="Request URL" />
-            <button onClick={props.Send} className="px-11 py-3 bg-pink text-white rounded-full font-bold">
-                SEND
-            </button>
-        </div>
-    )
+      <input
+        type={"text"}
+        value={props.Value}
+        onChange={props.OnFunc}
+        className="text-white bg-transparent w-4/6 pl-2"
+        placeholder="Request URL"
+      />
+      <button
+        onClick={props.Send}
+        className="w-1/6 py-3 bg-pink text-white rounded-full font-bold flex align-center justify-center"
+      >
+        SEND &nbsp;
+        <SendIcon Width="25" Height="25" />
+      </button>
+    </div>
+  )
 }
-
 
 export default RequestURL
