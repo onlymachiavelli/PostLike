1804ab6c55bcd9ae2662b28d46ba26844c11df66
onlymachiavelli <tmakaveli643@gmail.com>
2023-01-13 22:26:48 +0100

    preparing the row comp with the inuts

--- src/components/data.column.tsx
+++ src/components/data.column.tsx
@@ -1,15 +1,32 @@
 import React from "react"
 
-const DataColumn = () => {
+import { Trash } from "./svg"
+const DataColumn = ({ ...props }) => {
   return (
     <tr>
-      <td className="text-white bg-ddark p-3 text-center w-1/3 text-sm">
-        REQUEST PARAMS
+      <td className="text-white bg-ddark  text-center w-1/3 text-sm">
+        <input
+          type={"text"}
+          value={props.Value}
+          onChange={props.OnFunc}
+          className="text-white bg-transparent w-full pl-3"
+          placeholder="Data Key"
+        />
       </td>
-      &nbsp;
-      <td className="text-white bg-ddark p-3 text-center text-sm">VALUE</td>
-      &nbsp;
-      <td className="text-white bg-pink p-3 text-center text-sm">DEL</td>
+      <td className="text-white bg-ddark p-3 text-center text-sm">
+        <input
+          type={"text"}
+          value={props.Value}
+          onChange={props.OnFunc}
+          className="text-white bg-transparent w-full"
+          placeholder="Data Value"
+        />
+      </td>
+      <td className="text-white bg-pink p-3 text-center text-sm">
+        <button>
+          <Trash Width="20" Height="20" />
+        </button>
+      </td>
     </tr>
   )
 }
--- src/pages/Application.tsx
+++ src/pages/Application.tsx
@@ -29,11 +29,9 @@ const Application = () => {
               <td className="text-white bg-ddark p-3 text-center w-1/3 text-sm">
                 REQUEST PARAMS
               </td>
-              &nbsp;
               <td className="text-white bg-ddark p-3 text-center text-sm">
                 VALUE
               </td>
-              &nbsp;
               <td className="text-white bg-pink p-3 text-center text-sm">
                 DEL
               </td>
