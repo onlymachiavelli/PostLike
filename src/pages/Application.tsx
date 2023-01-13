385ce7b4cf30d30ef305ea93e9093f95bbba6655
onlymachiavelli <tmakaveli643@gmail.com>
2023-01-13 21:40:22 +0100

    sep

--- src/pages/Application.tsx
+++ src/pages/Application.tsx
@@ -16,8 +16,24 @@ const Application = () => {
       </nav>
 
       <div className="w-full h-5/6 flex align-center justify-center mt-20 gap-x-2">
-        <aside className="w-1/3 bg-dddark h-3/4">hello world</aside>
-        <aside className="w-3/5 bg-dddark h-3/4"></aside>
+        <aside className="w-1/3 bg-dddark h-3/4 rounded-sm ">
+          <div className="w-full h-auto p-3 bg-ddark rounded-sm overflow-hidden">
+            <div className="flex w-full h-auto  align-center  justify-between">
+              <p className="text-white font-bold">REQUEST QUERY</p>
+              <p className="text-white text-sm pt-1">Method : GET</p>
+            </div>
+          </div>
+        </aside>
+        <aside className="w-3/5 bg-dddark h-3/4 rounded-sm overflow-hidden">
+          <div className="w-full h-auto p-3 bg-ddark rounded-sm overflow-hidden">
+            <div className="flex w-full h-auto  align-center  justify-between">
+              <p className="text-white font-bold">RESPONSE BOX</p>
+              <p className="text-pink text-sm pt-1">
+                STATUS:<p className="text-green"> 200 (OK)</p>
+              </p>
+            </div>
+          </div>
+        </aside>
       </div>
     </div>
   )
