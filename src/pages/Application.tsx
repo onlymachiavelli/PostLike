0b1bb90e140efa1df363b379bbcc427f177aa558
onlymachiavelli <tmakaveli643@gmail.com>
2023-01-13 21:23:18 +0100

    preparing the blocks

--- src/pages/Application.tsx
+++ src/pages/Application.tsx
@@ -7,17 +7,17 @@ import Navbar from "../components/navbar"
 import RequestURL from "../components/Request.Url"
 const Application = () => {
   return (
-    <div>
-      <nav className="w-full h-auto p-5">
+    <div className="w-full h-screen overflow-hidden">
+      <nav className="w-full p-5 h-auto">
         <Navbar />
         <div className="w-full flex justify-center mt-10">
           <RequestURL />
         </div>
       </nav>
 
-      <div className="w-full h-auto flex align-center justify-center">
-        <aside className=""></aside>
-        <aside className=""></aside>
+      <div className="w-full h-5/6 flex align-center justify-center mt-20 gap-x-2">
+        <aside className="w-1/3 bg-dddark h-3/4">hello world</aside>
+        <aside className="w-3/5 bg-dddark h-3/4"></aside>
       </div>
     </div>
   )
