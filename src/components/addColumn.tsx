435dc53301e51c23ae13bb126843902cda7d9474
onlymachiavelli <tmakaveli643@gmail.com>
2023-01-15 00:53:54 +0100

    got rid of datatype never

--- src/components/addColumn.tsx
+++ src/components/addColumn.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 
 import DataColumn from "./data.column"
 const Fields = () => {
-  const [fields, setFields] = useState([])
+  const [fields, setFields]: any = useState([])
   const addField = () => {
     setFields([...fields, <DataColumn />])
   }
