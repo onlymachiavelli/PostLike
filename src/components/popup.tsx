9deb1ec4d2ccc82a3b9711693409090a1bc27d64
MACHIAVELLI <tmakaveli643@gmail.com>
2023-01-19 13:22:24 +0100

    preparing button changing method

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react"
 
 
 
+export const [Visible, setVisible] = useState(false)
 export const useSetMethod = () =>{
   
   //get by default
@@ -18,27 +19,40 @@ export const useSetMethod = () =>{
 
 const PopUp = ({ ...props }) => {
   return (
-    <div className="w-full h-full bg-dark/80 fixed absolute top-0 flex align-center justify-center ">
+    <div className={`w-full h-full bg-dark/80 fixed absolute top-0 flex align-center justify-center ${Visible ? "visible" : "invisible"}`}>
       <div className="w-1/3 m-auto  bg-dddark h-auto p-10 text-center rounded">
         <p className="text-white font-bold">Choose One Method</p>
         <br />
         <div className=" flex gap-x-4">
           <button
-            onClick={props.cMethod}
+            onClick={()=>{
+              useSetMethod().setMethod("GET")
+
+              setVisible(false)
+
+            }}
             className="w-1/3 py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
+            
           >
             GET
           </button>
 
           <button
-            onClick={props.cMethod}
+            onClick={()=>{
+              useSetMethod().setMethod("POST")
+              setVisible(false)
+            }}
             className="w-1/3  py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
           >
             POST
           </button>
 
           <button
-            onClick={props.cMethod}
+            onClick={()=>{
+              useSetMethod().setMethod("DELETE")
+              setVisible(false)
+
+            }}
             className="w-1/3  py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
           >
             DELETE
@@ -47,14 +61,21 @@ const PopUp = ({ ...props }) => {
         <br />
         <div className=" flex gap-x-4">
           <button
-            onClick={props.cMethod}
+            onClick={()=>{
+              useSetMethod().setMethod("PUT")
+              setVisible(false)
+
+            }}
             className="w-1/2 py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
           >
             PUT
           </button>
 
           <button
-            onClick={props.cMethod}
+            onClick={()=>{
+              useSetMethod().setMethod("PATCH")
+              setVisible(false)
+            }}
             className="w-1/2 py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
           >
             PATCH
