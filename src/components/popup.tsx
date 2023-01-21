26c65c1d5003eef7301e512c611643b6a32b1ee3
MACHIAVELLI <tmakaveli643@gmail.com>
2023-01-21 11:55:19 +0100

    removed some ers

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -1,35 +1,13 @@
 import React, {useState, useEffect} from "react"
-
-
-
-export const [Visible, setVisible] = useState(false)
-export const useSetMethod = () =>{
-  
-  //get by default
-  const [method, setM] = useState("GET")
-  const setMethod = (e:any) =>{
-    setM(e.target.innerText)
-  }
-
-
-
-
-  return {method, setMethod, setM}
-}
-
 const PopUp = ({ ...props }) => {
   return (
-    <div className={`w-full h-full bg-dark/80 fixed absolute top-0 flex align-center justify-center ${Visible ? "visible" : "invisible"}`}>
+    <div className={`w-full h-full bg-dark/80 fixed absolute top-0 flex align-center justify-center ${props.Visibility}`}>
       <div className="w-1/3 m-auto  bg-dddark h-auto p-10 text-center rounded">
         <p className="text-white font-bold">Choose One Method</p>
         <br />
         <div className=" flex gap-x-4">
           <button
             onClick={()=>{
-              useSetMethod().setMethod("GET")
-
-              setVisible(false)
-
             }}
             className="w-1/3 py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
             
@@ -39,8 +17,6 @@ const PopUp = ({ ...props }) => {
 
           <button
             onClick={()=>{
-              useSetMethod().setMethod("POST")
-              setVisible(false)
             }}
             className="w-1/3  py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
           >
@@ -49,8 +25,6 @@ const PopUp = ({ ...props }) => {
 
           <button
             onClick={()=>{
-              useSetMethod().setMethod("DELETE")
-              setVisible(false)
 
             }}
             className="w-1/3  py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
@@ -62,8 +36,6 @@ const PopUp = ({ ...props }) => {
         <div className=" flex gap-x-4">
           <button
             onClick={()=>{
-              useSetMethod().setMethod("PUT")
-              setVisible(false)
 
             }}
             className="w-1/2 py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
@@ -73,8 +45,6 @@ const PopUp = ({ ...props }) => {
 
           <button
             onClick={()=>{
-              useSetMethod().setMethod("PATCH")
-              setVisible(false)
             }}
             className="w-1/2 py-3 bg-pink m-auto text-white rounded   font-bold flex items-center justify-center"
           >
